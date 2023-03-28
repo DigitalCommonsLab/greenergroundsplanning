96611d3302582747d276636c0955c59daa9b1e54
napo <napo@fbk.eu>
2023-03-28 11:16:40 +0200

    Bologna trees

---
+++ data/get.sh
@@ -0,0 +1,1 @@
+wget "https://opendata.comune.bologna.it/api/explore/v2.1/catalog/datasets/alberi-manutenzioni/exports/csv?lang=it&timezone=Europe%2FRome&use_labels=true&delimiter=%3B" -O bologna_trees.csv
